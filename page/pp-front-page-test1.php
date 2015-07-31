b50f382b5937d1928a982fac4eefe8bbdb4035e3
aldelpech <al.delpech@parcours-performance.com>
2015-07-31 10:02:20 +0200

    nice layout for a test div hor-item

--- page/pp-front-page-test1.php
+++ page/pp-front-page-test1.php
@@ -14,8 +14,131 @@ get_header(); // Loads the header.php template. ?>
 	</section>
 	<!-- End featured area. -->
 
+	<section class="flexbox-horizontal">
+		<h3>Les derniers articles du site, horizontal 2 colonnes</h3>	
+		
+
+		<div class="hor-masonry">
+			<div class="hor-item">
+				<h2>Attention</h2>
+				<p>see http://w3bits.com/css-masonry/<p>
+				<p>As CSS columns are supported by modern browsers only, you may also consider adding JavaScript fallbacks to make it work on older browsers. We can add a fallback to our masonry by making use of jQuery Masonry plugin.<p>
+
+				<p>Following is the workaround to call jQuery Masonry as a fallback to our CSS masonry in IE9 or below:<p>
+
+				<!-- will render -->
+				<!--[if lte IE 9]>
+				<script src="masonry.pkgd.min.js"></script>
+				<![endif]-->
+				<!-- used http://www.freeformatter.com/html-escape.html#ad-output -->
+				<pre><code>
+				&lt;!--[if lte IE 9]&gt;
+				&lt;script src=&quot;masonry.pkgd.min.js&quot;&gt;&lt;/script&gt;
+				&lt;![endif]--&gt;
+				</code></pre>
+			</div>
+			<div class="hor-item">
+				<div class="demo user-profile">
+				  <img src="http://api.randomuser.me/0.3.2/portraits/men/7.jpg" alt="" />
+				  <!-- <h3>John Doe</h3> -->
+				  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum itaque nam blanditiis eveniet enim eligendi quae adipisci? <p>Assumenda blanditiis voluptas tempore porro quibusdam beatae deleniti quod asperiores sapiente dolorem error! Quo nam quasi soluta reprehenderit laudantium optio ipsam ducimus consequatur enim fuga quibusdam mollitia nesciunt modi.</p>
+				  <p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ipsam officia autem tenetur odit reprehenderit amet. Ut voluptatibus eum dolorem suscipit ipsa veniam animi dolore officia sunt magni assumenda vitae!
+
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium repellat cumque beatae aliquam earum odio. Earum corporis necessitatibus minus aut culpa tempore deleniti laudantium repellat autem consequuntur est quos doloribus.
+				  </p>
+				 
+				</div>
+				<footer>Demonstration by Sara Soueidan. <a href="http://alistapart.com/article/css-shapes-101">See article</a> on A List Apart.</footer>
+		</div>
+			
+			<!-- voir http://demosthenes.info/blog/844/Easy-Masonry-Layout-With-Flexbox -->
+				<!-- Begin excerpts area. -->
+		<?php 
+		// set the "paged" parameter (use 'page' if the query is on a static front page)
+		$paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
+
+		$loop = new WP_Query(
+			array(
+				'post_type' => 'post',
+				'posts_per_page' => 10,
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'post_format',
+						'field' => 'slug',
+						'terms' => array(
+							'post-format-aside',
+							'post-format-audio',
+							'post-format-chat',
+							'post-format-gallery',
+							'post-format-image', 
+							'post-format-link', 
+							'post-format-quote',
+							'post-format-status',
+							'post-format-video'
+						),
+						'operator' => 'NOT IN'
+					)
+				),
+				'post__not_in' => $do_not_duplicate,
+				'paged' 			=> $paged, 
+				'category__not_in'	=> '72', /* exclude category "AIDE" */
+				'post_status' => 'publish', /* show only published posts */
+			)
+		); ?>
+
+		<?php if ( $loop->have_posts() ) : ?>
+
+			<?php while ( $loop->have_posts() ) : $loop->the_post(); $do_not_duplicate[] = get_the_ID();  ?>
+
+				<div class="hor-item">
+					<figure>
+						<?php if ( current_theme_supports( 'get-the-image' ) ) get_the_image( array( 'meta_key' => 'Thumbnail', 'size' => 'medium' ) ); ?>
+					</figure>
+					<div class="categorie">
+							<span class="categories"><?php the_category(', '); ?></span>
+					</div>
+					<figcaption>
+						<header class="entry-header">
+							<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title tag="h3"]' ); ?>
+						</header><!-- .entry-header -->
+						<div class="entry-summary">
+							<?php the_excerpt(); ?>
+						</div><!-- .entry-summary -->
+						<p class="entry-meta">
+						<span class="categories">  </span>
+						<?php echo apply_atomic_shortcode( 'entry_byline', '<span class="entry-byline">' . __( '[entry-published] [entry-edit-link before=" | "]', 'unique' ) . '</span>' ); ?>
+					</figcaption>
+					
+					<!-- <div class="clearfix"></div> -->
+				</div>	
+						
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+		</div><!-- #pp-container -->
+
+		<!-- http://wordpress.stackexchange.com/questions/174907/how-to-use-the-posts-navigation-for-wp-query-and-get-posts -->
+		<?php 
+		$GLOBALS['wp_query']->max_num_pages = $loop->max_num_pages;
+		
+		// echos the return of get_the_posts_pagination()
+		the_posts_pagination( array(
+			'mid_size' => 2,
+			'prev_text' => __( 'Plus récents', 'clea-base' ),
+			'next_text' => __( 'Plus anciens', 'clea-base' ),
+			'screen_reader_text' => __( 'autres articles', 'clea-base' ),
+		) ); ?>
+		
+		
+		<?php wp_reset_query(); ?>
+
+	</section>	
+	
+	
+	
 	<section class="flexbox-list">
-		<h3>Les derniers articles du site</h3>	
+		<h3>Les derniers articles du site, en colonnes</h3>	
 		<div class="masonry">
 			<div class="item">
 				<h2>Attention</h2>
